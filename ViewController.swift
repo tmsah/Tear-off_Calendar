34d9d9dab18092365107d9e694a32410b3619705
tmsah <hideki81000@gmail.com>
2019-11-13 22:30:19 +0900

    日付の調整（本番用）

--- ViewController.swift
+++ ViewController.swift
@@ -33,8 +33,8 @@ class ViewController: UIViewController {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
         dateFormatter.locale = NSLocale(localeIdentifier: "ja_JP") as Locale
-        firstDay = dateFormatter.date(from: "2018-12-31")!
-        lastDay = dateFormatter.date(from: "2020-01-01")!
+        firstDay = dateFormatter.date(from: "2019-12-31")!
+        lastDay = dateFormatter.date(from: "2021-01-01")!
 
         dateFormatter.dateFormat = "MM"
         let month = dateFormatter.string(from: thisDay)
